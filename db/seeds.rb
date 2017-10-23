790f51285e48569a6d10ece4ea157eb06ceeedd3
Philip Nguyen <vietwhammies@gmail.com>
2017-10-23 12:50:26 -0700

    Seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,30 +8,30 @@
 
 Category.delete_all
 
-Category.create(name:'Meats',
+Category.create(id: 1, name:'Meats',
   image_url: 'meats/meats.jpg')
-Category.create(name:'Beverages',
+Category.create(id: 2,name:'Beverages',
   image_url: 'beverages/beverages.JPG')
-Category.create(name:'Breads',
+Category.create(id: 3,name:'Breads',
   image_url: 'bread/bread2.jpg')
-Category.create(name:'Breakfast',
+Category.create(id: 4,name:'Breakfast',
   image_url: 'breakfast/breakfast.JPG')
-Category.create(name:'Condiments',
+Category.create(id: 5,name:'Condiments',
   image_url: 'condiments/condiments.JPG')
-Category.create(name:'Fruits',
+Category.create(id: 6,name:'Fruits',
   image_url: 'fruits/fruits.JPG')
-Category.create(name:'Dairy',
+Category.create(id: 7,name:'Dairy',
   image_url: 'milk-dairy/milk-dairy.JPG')
-Category.create(name:'snacks',
+Category.create(id: 8,name:'snacks',
   image_url: 'snacks/snacks.JPG')
-Category.create(name:'Vegetables',
+Category.create(id: 9,name:'Vegetables',
   image_url: 'vegetables/vegetables.JPG')
 
 
-Product.delete_all
+#Product.delete_all
 
 Product.create(title:'Cereal in a Bowl',
-  category_id:2,
+  category_id:4,
   description:
     %{
       Frosted Flakes (supposedly) in a very aesthetic bowl which is hopefully
