9cdded189738ad08f80c10d3e8f42b2a4cc4817e
Jamal Arif <jamala@Jamals-MacBook-Pro-2.local>
2017-10-25 12:58:50 -0700

    Added recipies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022073717) do
+ActiveRecord::Schema.define(version: 20171025182152) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171022073717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "recipes"
   end
 
   create_table "users", force: :cascade do |t|
