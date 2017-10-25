f84c393e3eee7f2bf7d88b62cba40d64d4dc8eac
Paul <paul.t.nguyen@sjsu.edu>
2017-10-25 10:33:33 -0700

    Added Guest login

--- db/migrate/20171020092340_devise_create_users.rb
+++ db/migrate/20171020092340_devise_create_users.rb
@@ -3,7 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: "123456"
 
       ## Recoverable
       t.string   :reset_password_token
