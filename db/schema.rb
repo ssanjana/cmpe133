2c2c241114400e958bdc3ce8861331647f8b650d
Michael Li <mikeli.fire@yahoo.com>
2017-11-01 13:01:06 -0700

    Added savings

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20171030192941) do
     t.string "image_url"
   end
 
-  create_table "guests", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_guests_on_email", unique: true
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -42,7 +35,7 @@ ActiveRecord::Schema.define(version: 20171030192941) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password", default: "123456", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -55,7 +48,7 @@ ActiveRecord::Schema.define(version: 20171030192941) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.integer "card"
-    t.boolean "guest"
+    t.boolean "guest", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
