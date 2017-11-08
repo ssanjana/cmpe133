ce91ec29d93a85e8fc37fbb95732ecbc02dad3b3
Philip Nguyen <vietwhammies@gmail.com>
2017-11-08 11:00:15 -0800

    Made Cart better

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,12 +2,19 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   def add_item(product_id)
-    item = items.where('product_id = ?', product_id).first
+    item = line_items.find_by_product_id(product_id)
+    #item = items.find_by(product_id: product_id).first
+    #item = items.find_by(product_id: product_params[:product][:product_id])
     if item
-      item.quantity + 1
+      item.quantity += 1
       save
     else
-      cart.items << Line_Item.new(product_id: product_id, quantity: 1, cart_id: self.id)
+      item = line_items.build(product_id: product_id, quantity: 1)
+      #cart.items << Line_Item.new(product_id: product_id, quantity: 1, cart_id: self.id)
     end
+    item
+  end
+  # for loop iterating through all the line_items to add up for a cart subtotal
+  def subtotal
   end
 end
