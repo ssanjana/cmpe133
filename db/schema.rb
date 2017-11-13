12628ef1f0dc49d67ad357e838d0542cd7441af3
DennisLi0705 <dennislee0705@gmail.com>
2017-11-13 10:34:06 -0800

    Added Cart UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108044137) do
+ActiveRecord::Schema.define(version: 20171106190748) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20171108044137) do
   create_table "line_items", force: :cascade do |t|
     t.integer "product_id"
     t.integer "cart_id"
-    t.integer "quantity", default: 1
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
