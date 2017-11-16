070f67f09e4b07cbc6ab9c970dd71a6cedff5b57
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2017-11-15 18:07:29 -0800

    Forgot to add files

---
+++ app/models/search.rb
@@ -0,0 +1,16 @@
+class Search < ApplicationRecord
+    def result
+        @result ||= find_result
+      end
+      
+      private
+      
+      def find_result
+        result = Product.order(:title)
+        result = result.where("title like ?", "%#{keywords}%") if keywords.present?
+        result = result.where(category_name: category_name) if category_name.present?
+        result = result.where("price >= ?", min_price) if min_price.present?
+        result = result.where("price <= ?", max_price) if max_price.present?
+        result
+      end
+end
