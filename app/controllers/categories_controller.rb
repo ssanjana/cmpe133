53f4adcca397df1bd057ac5739188d6cf14011ed
Paul <paul.t.nguyen@sjsu.edu>
2017-11-18 16:47:38 -0800

    Fixed some shit with controller searching

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,9 +22,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
-    @categories = Category.search(params[:search])
     @products = @category.products
-    @products = Product.search(params[:search])
   end
 
   private
