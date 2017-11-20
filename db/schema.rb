5105c6e651109f58ca03e3791afe29386b4d120c
Paul <paul.t.nguyen@sjsu.edu>
2017-11-20 09:04:14 -0800

    Fixed links, added fancy buttons

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20171120035549) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.integer "card"
-    t.boolean "guest", default: true
+    t.boolean "guest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
